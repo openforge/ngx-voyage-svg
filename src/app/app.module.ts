a342b3b27ca9faa663239951e96e7bfa2f94440e
Khaled Shaaban <khaled@webjunto.com>
2018-02-13 17:21:33 -0500

    chore(demo): setup basic demo app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
-
+import { VoyageNavigationModule } from './voyage-navigation/voyage-navigation.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, VoyageNavigationModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
