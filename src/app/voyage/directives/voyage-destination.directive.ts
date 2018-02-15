db9bbdb0315716b1af9bfa81d92534220283f3b9
Khaled Shaaban <khaled@webjunto.com>
2018-02-15 18:54:04 -0500

    fix(stops): trigger Change Detection on Destination isReached changes

--- src/app/voyage/directives/voyage-destination.directive.ts
+++ src/app/voyage/directives/voyage-destination.directive.ts
@@ -1,4 +1,11 @@
-import { Directive, OnInit, Input, ElementRef } from '@angular/core';
+import {
+  Directive,
+  OnInit,
+  OnDestroy,
+  Input,
+  ElementRef,
+  ChangeDetectorRef
+} from '@angular/core';
 import { Subscription } from 'rxjs/Subscription';
 import { filter } from 'rxjs/operators';
 
@@ -8,8 +15,8 @@ import { VoyagePathService } from './../services/voyage-path.service';
   selector: '[voyageDestination]',
   exportAs: 'voyageDestination',
 })
-export class VoyageDestinationDirective implements OnInit {
-  @Input() public point: {x: number, y: number};
+export class VoyageDestinationDirective implements OnInit, OnDestroy {
+  @Input() public point: { x: number; y: number };
   @Input() public requiredProgress: number;
 
   private positionSubscription: Subscription;
@@ -23,14 +30,21 @@ export class VoyageDestinationDirective implements OnInit {
 
   constructor(
     private elRef: ElementRef,
-    private voyagePathService: VoyagePathService
+    private voyagePathService: VoyagePathService,
+    private cdRef: ChangeDetectorRef
   ) {}
 
   public ngOnInit() {
     this.positionSubscription = this.voyagePathService.currentPosition$
       .pipe(filter(() => !this.isReached))
       .subscribe(currentPosition => {
-        this.isReached = currentPosition.lengthAtPoint >= this.pathLengthAtPoint;
+        this.isReached =
+          currentPosition.lengthAtPoint >= this.pathLengthAtPoint;
+        this.cdRef.detectChanges();
       });
+  }
+
+  public ngOnDestroy() {
+    this.positionSubscription.unsubscribe();
   }
 }
